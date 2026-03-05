dca07f1524a1b8c6f689e42db642241e4f188130
nexxovate-shaikh <nexxovate@gmail.com>
2026-03-05 20:09:31 +0530

    Update DB

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,9 +1,9 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
+const uri = process.env.MONGODB_URI as string;
 
 if (!uri) {
-  throw new Error("Please add MONGODB_URI to env");
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
 
 let client: MongoClient;
@@ -13,16 +13,15 @@ declare global {
   var _mongoClientPromise: Promise<MongoClient>;
 }
 
-if (!global._mongoClientPromise) {
-
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
   client = new MongoClient(uri);
-
-  global._mongoClientPromise =
-    client.connect();
-
+  clientPromise = client.connect();
 }
-
-clientPromise =
-  global._mongoClientPromise;
 
 export default clientPromise;
